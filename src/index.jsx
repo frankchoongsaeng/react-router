5535bde9326465efb9292db1b8ebd214ecc6142e
Frank Choongsaeng <frankchoongsaeng@gmail.com>
2021-06-07 12:33:43 +0000

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// stylesheets
+import './styles/main.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
